0bceec8d4c850539622af3925d018ab1c1aef0b6
AlwaysAwake <tellmewhy07@korea.ac.kr>
2016-11-19 00:32:08 +0900

    화장실 지도에 출처 추가함

--- src/components/Toilet/ToiletMap.js
+++ src/components/Toilet/ToiletMap.js
@@ -6,6 +6,7 @@ const ToiletMap = () => {
     <div className="toilet-container">
       <h5>화장실 지도(서울)</h5>
       <img src="/images/toilet_map.png" role="presentation" />
+      <h6 style={{ float: 'right' }}>출처: 박원순 서울시장 트위터</h6>
     </div>
   );
 }
