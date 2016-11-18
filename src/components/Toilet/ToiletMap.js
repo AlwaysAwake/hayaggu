a30f3e0f5069b20474f697b00aaec3919b5093de
AlwaysAwake <tellmewhy07@korea.ac.kr>
2016-11-19 00:32:46 +0900

    화장실 지도 제목 바꿈

--- src/components/Toilet/ToiletMap.js
+++ src/components/Toilet/ToiletMap.js
@@ -4,7 +4,7 @@ import React from 'react';
 const ToiletMap = () => {
   return (
     <div className="toilet-container">
-      <h5>화장실 지도(서울)</h5>
+      <h5>화장실 지도(서울광장 주변)</h5>
       <img src="/images/toilet_map.png" role="presentation" />
       <h6 style={{ float: 'right' }}>출처: 박원순 서울시장 트위터</h6>
     </div>
