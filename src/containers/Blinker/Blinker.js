bff0d67d3a269df5422fb20a126a8f1023570d16
AlwaysAwake <tellmewhy07@korea.ac.kr>
2016-11-25 21:53:33 +0900

    Added length limit

--- src/containers/Blinker/Blinker.js
+++ src/containers/Blinker/Blinker.js
@@ -6,6 +6,8 @@ import RaisedButton from 'material-ui/RaisedButton';
 
 import * as Actions from '../../actions/actions';
 
+const maximumCatchwordLength = 20;
+
 const requestFullScreen = () => {
   const docElm = document.documentElement;
   if (docElm.requestFullscreen) {
@@ -34,9 +36,13 @@ class Blinker extends Component {
   }
 
   onClickGoBlinker() {
-    this.setState({ blinkerOpened: true });
-    this.props.dispatch(Actions.addCatchwordCount({ content: this.state.catchwordText }));
-    requestFullScreen();
+    if (this.state.catchwordText.length > maximumCatchwordLength) {
+      alert(`${maximumCatchwordLength}글자 내로 함축적으로 요약해보세요! 당신의 센스를 기대합니다^^`);
+    } else {
+      this.setState({ blinkerOpened: true });
+      this.props.dispatch(Actions.addCatchwordCount({ content: this.state.catchwordText }));
+      requestFullScreen();
+    }
   }
 
   render() {
@@ -51,11 +57,12 @@ class Blinker extends Component {
         <div className="mdl-grid">
           <div className="mdl-cell mdl-cell--12-col mdl-cell--12-col-tablet mdl-cell--12-col-phone">
             <TextField
-              floatingLabelText="전광판에 띄울 문구를 작성해주세요."
+              floatingLabelText={`전광판에 띄울 문구를 작성해주세요. (${maximumCatchwordLength}자 이내)`}
               onChange={e => this.setState({ catchwordText: e.target.value })}
               style={{ width: '100%', display: 'inline-block' }}
               value={this.state.catchwordText}
             />
+            {`${this.state.catchwordText.length}자 / ${maximumCatchwordLength}자`}
             <RaisedButton
               label="전광판 띄우기"
               onTouchTap={() => this.onClickGoBlinker()}
